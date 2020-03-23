d2239782ff7db9a94b2466a175e4164127a0baf6
Rajkumar Dusad <32831684+rajkumardusad@users.noreply.github.com>
2020-03-23 15:00:42 +0530

    Flash Framework

---
+++ application/templates/home.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct access allowed');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Home</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
+  <style>
+body{
+  width:100%;
+  font-family:'arial';
+  background:#fff;
+}
+* {
+  margin:0 auto;
+}
+h1{
+  font-size:25px;
+  color:#273f59;
+  text-align:center;
+  margin-top:30vh;
+}
+</style>
+</head>
+<body>
+  <h1>Oops, it's working!</h1>
+</body>
+</html>
