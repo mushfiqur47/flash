c7f593019cb24e36a93a6c63e63ddfcefd7f057e
Rajkumar Dusad <32831684+rajkumardusad@users.noreply.github.com>
2020-05-31 14:11:09 +0530

    Flash Framework

--- application/app/views.php
+++ application/app/views.php
@@ -6,13 +6,19 @@ require_once('models.php');
 //Create your views here.
 
 class app_view extends Views{
+
+  function __construct() {
+    parent::__construct();
+  }
+
   //home view
   function home() {
+    //render home template
     return $this->render("home");
   }
 
   //custom 404 error handler view
   function page_not_found($request) {
-    return $this->response("404 Page Not Found",404);
+    return $this->response("404 Page Not Found", 404);
   }
 }
